ca2ab59903c96378656fc3e9bb4ec3bf3df86ab3
VividCoder <30574877+VividCoder@users.noreply.github.com>
2020-01-18 22:53:50 +0000

    initial commit.

---
+++ TrinityStudio/Forms/Feedback/ConsoleOutput.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrinityStudio.Forms.Feedback
+{
+    public partial class ConsoleOutput : Form
+    {
+
+        public List<string> Lines = new List<string>();
+
+        public ConsoleOutput()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void DebugMsg(string text)
+        {
+            Lines.Add(text);
+            ///
+            //Text = Text + text + "\n";
+        //    richTextBox1.Text = richTextBox1.Text + text + "\n";
+        }
+
+
+    }
+}
---
+++ TrinityStudio/TrinityIDE.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TrinityStudio.Forms;
+using TrinityStudio.Forms.Feedback;
+using TrinityStudio.Forms.Browser.Content;
+using WeifenLuo;
+using WeifenLuo.WinFormsUI;
+using WeifenLuo.WinFormsUI.Docking;
+namespace TrinityStudio
+{
+    public partial class TrinityIDE : Form
+    {
+        public static ConsoleOutput FormConsoleOutput;
+        public static ContentBrowser FormContentBrowse;
+        public TrinityIDE()
+        {
+            InitializeComponent();
+            //    FormConsoleOutput = new ConsoleOutput();
+            //      FormConsoleOutput.Show(this.dockPanel1, DockState.DockBottom);
+
+            //      FormConsoleOutput.DebugMsg("Engine initiated.");
+            FormConsoleOutput = new ConsoleOutput();
+            FormConsoleOutput.Show();
+            this.dockingManager1.DockControl(FormConsoleOutput, this, Syncfusion.Windows.Forms.Tools.DockingStyle.Bottom, 150);
+
+            FormContentBrowse = new ContentBrowser();
+            FormContentBrowse.Show();
+            this.dockingManager1.DockControl(FormContentBrowse, FormConsoleOutput, Syncfusion.Windows.Forms.Tools.DockingStyle.Tabbed, 150);
+
+        }
+
+        private void dockPanel1_ActiveContentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dockingClientPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
